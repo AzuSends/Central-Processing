4fb00e5d7179beb6f4f44c226f2ed6a92fe2f154
Benjamin Silver <besilver@ucsc.edu>
2025-11-05 15:10:59 -0800

    Added deck's and word display

--- src/scene/Game.js
+++ src/scene/Game.js
@@ -5,29 +5,65 @@ class Game extends Phaser.Scene {
 
     preload() {
       this.load.image('cardBack', './assets/cardBack.png');
+      this.gameWidth = this.sys.game.canvas.width
+      this.gameHeight = this.sys.game.canvas.height
+
+
     }
 
     create() {
-      this.deckOccipitalWords = ["o1", "o2", "o3"]
-      this.deckAuditoryWords = ["a1", "a2", "a3"]
-      this.deckMotorWords = ["m1", "m2", "m3"]
+      this.add.text(this.gameWidth / 2, 75, 'Central Processing', {
+        fontSize: '48px',
+        fill: '#fff',
+        fontFamily: 'Times New Roman',
+        }).setOrigin(0.5);
+      this.add.text(this.gameWidth / 4, 145, 'Occipital', {
+        fontSize: '32px',
+        fill: '#fff',
+        fontFamily: 'Times New Roman',
+      }).setOrigin(0.5);
+      this.add.text(this.gameWidth / 2, 145, 'Auditory', {
+      fontSize: '32px',
+      fill: '#fff',
+      fontFamily: 'Times New Roman',
+      }).setOrigin(0.5);
+      this.add.text(this.gameWidth * .75, 145, 'Motor', {
+      fontSize: '32px',
+      fill: '#fff',
+      fontFamily: 'Times New Roman',
+      }).setOrigin(0.5);
+      this.currWordText = this.add.text(this.gameWidth / 2, 500, '', {
+        fontSize: '32px',
+        fill: '#fff',
+        fontFamily: 'Times New Roman',
+      }).setOrigin(0.5);
+      this.add.text(this.gameWidth / 2, 450, 'Current Word:', {
+        fontSize: '24px',
+        fill: '#fff',
+        fontFamily: 'Times New Roman',
+      }).setOrigin(0.5);
+
+      this.currWord = ""
+      this.deckOccipitalWords = ["Party", "Classroom", "Dragon", "Studying", "Dancing", "Flying", "Falling", "Flamingo", "Bitting", "Puzzle", "Outer Space", "Acid", "Disco Ball"]
+      this.deckAuditoryWords = ["Disco Ball", "Acid", "Bitting", "Eating", "Book", "Toddler", "Spicy", "Soccer", "Tree", "Fax Machine", "Arcade Machine", "Ace of Spades", "Chariot"]
+      this.deckMotorWords = ["Moon Landing", "Rock Climbing", "Grasshopper", "Bowling Pin", "Orbit", "Wizard", "Party", "Rock n Roll", "Antartica", "Skull", "Hail", "Sandwich", "Banana Slug", ]
 
 
-      this.deckOccipital =  this.add.sprite(300, 300, "cardBack").setInteractive();
+      this.deckOccipital =  this.add.sprite(this.gameWidth / 4,  this.gameHeight / 3, "cardBack").setInteractive();
       this.deckOccipital.scale = .15
       this.deckOccipital.on('pointerdown', () => {
-        console.log("clicked occipital")
+        this.currWord = this.deckOccipitalWords[Math.floor(Math.random() * this.deckOccipitalWords.length)]
       })
-      this.deckAuditory =  this.add.sprite(500, 300, "cardBack").setInteractive();
+      this.deckAuditory =  this.add.sprite(this.gameWidth / 2,  this.gameHeight / 3, "cardBack").setInteractive();
       this.deckAuditory.scale = .15
       this.deckAuditory.on('pointerdown', () => {
-        console.log("clicked Auditory")
+        this.currWord = this.deckAuditoryWords[Math.floor(Math.random() * this.deckAuditoryWords.length)]
       })
 
-      this.deckMotor =  this.add.sprite(700, 300, "cardBack").setInteractive();
+      this.deckMotor =  this.add.sprite(this.gameWidth * .75,  this.gameHeight / 3, "cardBack").setInteractive();
       this.deckMotor.scale = .15
       this.deckMotor.on('pointerdown', () => {
-        console.log("clicked Motor")
+        this.currWord = this.deckMotorWords[Math.floor(Math.random() * this.deckMotorWords.length)]
       })
 
 
@@ -35,7 +71,7 @@ class Game extends Phaser.Scene {
     }
 
     update() {
-
+      this.currWordText.text = this.currWord;
 
     }
 }
